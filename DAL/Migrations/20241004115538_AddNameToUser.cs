91d4289ef0049f3fe8e7c0dec55bdd66c6c640f6
MaxDelissen <maxdelissen17@gmail.com>
2024-10-11 09:25:57 +0200

    Db update

---
+++ DAL/Migrations/20241004115538_AddNameToUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Users",
+                type: "varchar(45)",
+                maxLength: 45,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Users");
+        }
+    }
+}
