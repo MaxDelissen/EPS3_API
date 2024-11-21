0737f23bd5a47ae5a08c6da63daa7d0af34de2a8
MaxDelissen <maxdelissen17@gmail.com>
2024-11-21 10:54:06 +0100

    Extra comment

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ namespace API
         public static void Main(string[] args)
         {
             //Comment to test PR workflow in GitHub TODO: Remove
+            //Extra comment to test PR workflow in GitHub TODO: Remove
 
             var builder = WebApplication.CreateBuilder(args);
 
